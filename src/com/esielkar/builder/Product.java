ef430521fca299f429178a3b04a40462e670a528
Esiel Arizmendi <esiel_kar@hotmail.com>
2022-06-25 23:24:09 -0500

    Creational Design Patterns: Builder - Adding an alternative Builder class

--- src/com/esielkar/builder/Product.java
+++ src/com/esielkar/builder/Product.java
@@ -22,4 +22,40 @@ public class Product {
         return "A: " + a + " B: " + b + " C: " + c;
     }
 
+    public static class ProductBuilder implements Builder {
+        private Product product;
+
+        public ProductBuilder() {
+            reset();
+        }
+
+        @Override
+        public void reset() {
+            product = new Product();
+        }
+
+        @Override
+        public Builder buildStepA() {
+            product.setA(true);
+            return this;
+        }
+
+        @Override
+        public Builder buildStepB() {
+            product.setB(true);
+            return this;
+        }
+
+        @Override
+        public Builder buildStepC() {
+            product.setC(true);
+            return this;
+        }
+
+        public Product build() {
+            return product;
+        }
+
+    }
+
 }
